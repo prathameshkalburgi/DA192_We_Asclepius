dfaaa3f7447f800b01e47f3f95f79e566b695175
pspanishetti <praveenpanishetti@gmail.com>
2020-08-01 13:29:44 +0530

    [Feat] - OnBoardActivity Created

--- app/src/main/java/in/asclepius/app/activities/OnBoardActivity.kt
+++ app/src/main/java/in/asclepius/app/activities/OnBoardActivity.kt
@@ -1,12 +1,60 @@
 package `in`.asclepius.app.activities
 
 import `in`.asclepius.app.R
+import `in`.asclepius.app.adapters.OnBoardPagerAdapter
+import `in`.asclepius.app.databinding.ActivityOnBoardBinding
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+
 
 class OnBoardActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityOnBoardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_board)
+        binding=ActivityOnBoardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setFeatureAdapter();
+        binding.nextFab.visibility= View.VISIBLE
+        binding.getStarted.visibility= View.GONE
+        binding.getStarted.setOnClickListener(View.OnClickListener { startActivity(Intent(this@OnBoardActivity,LoginActivity::class.java)) })
     }
+
+    private fun setFeatureAdapter() {
+        val adapter= OnBoardPagerAdapter(3,this)
+        binding.viewPager.adapter=adapter
+        binding.viewPager.setPageTransformer(MarginPageTransformer(25))
+
+        binding.nextFab.setOnClickListener(View.OnClickListener {
+            binding.viewPager.setCurrentItem(binding.viewPager.currentItem+1,true)
+            if(binding.viewPager.currentItem==2)
+            {
+                binding.nextFab.visibility= View.GONE
+                binding.getStarted.visibility= View.VISIBLE
+            }
+        })
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if(binding.viewPager.currentItem==2)
+                {
+                    binding.nextFab.visibility= View.GONE
+                    binding.getStarted.visibility= View.VISIBLE
+                }else{
+                    binding.nextFab.visibility= View.VISIBLE
+                    binding.getStarted.visibility= View.GONE
+                }
+            }
+        })
+
+    }
+
+
+
 }
--- app/src/main/java/in/asclepius/app/adapters/OnBoardPagerAdapter.kt
+++ app/src/main/java/in/asclepius/app/adapters/OnBoardPagerAdapter.kt
@@ -1,2 +1,106 @@
 package `in`.asclepius.app.adapters
 
+import `in`.asclepius.app.R
+import `in`.asclepius.app.databinding.OnboardFirstViewBinding
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.forEachIndexed
+import androidx.recyclerview.widget.RecyclerView
+
+class OnBoardPagerAdapter(
+    private val count: Int,
+    private val context: Context
+) : RecyclerView.Adapter<OnBoardPagerAdapter.ViewHolder>() {
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val binding = OnboardFirstViewBinding.bind(itemView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.onboard_first_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return count;
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        when (position) {
+            0 -> with(holder.binding) {
+                image.setAnimation(R.raw.find_doctors_anim)
+                featureText.text = context.resources.getString(R.string.on_board_message_three)
+                selectors.forEachIndexed { index, view ->
+                    if (index == position) {
+                        view.background.setTint(
+                            ContextCompat.getColor(
+                                context,
+                                R.color.colorAccent
+                            )
+                        )
+                    } else {
+                        view.background.setTint(
+                            ContextCompat.getColor(
+                                context,
+                                R.color.lightAccent
+                            )
+                        )
+                    }
+                }
+            }
+
+
+            1 -> with(holder.binding) {
+                image.setAnimation(R.raw.waiting_doctors_anim)
+                featureText.text = context.resources.getString(R.string.on_board_message_two)
+                selectors.forEachIndexed { index, view ->
+                    if (index == position) {
+                        view.background.setTint(
+                            ContextCompat.getColor(
+                                context,
+                                R.color.colorAccent
+                            )
+                        )
+                    } else {
+                        view.background.setTint(
+                            ContextCompat.getColor(
+                                context,
+                                R.color.lightAccent
+                            )
+                        )
+                    }
+                }
+            }
+
+            2 -> with(holder.binding) {
+                image.setAnimation(R.raw.opd_desk_anim)
+                featureText.text = context.resources.getString(R.string.on_board_message_one)
+                selectors.forEachIndexed { index, view ->
+                    if (index == position) {
+                        view.background.setTint(
+                            ContextCompat.getColor(
+                                context,
+                                R.color.colorAccent
+                            )
+                        )
+                    } else {
+                        view.background.setTint(
+                            ContextCompat.getColor(
+                                context,
+                                R.color.lightAccent
+                            )
+                        )
+                    }
+                }
+
+            }
+
+
+        }
+
+    }
+}
