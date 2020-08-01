4c23e70ea7e9a75fd1608b67d4c44d838d25a09a
pspanishetti <praveenpanishetti@gmail.com>
2020-08-01 15:12:32 +0530

    [Feat] - Book Appointment Feature Activity Work Started

--- app/src/main/java/in/asclepius/app/activities/BookAppointment.kt
+++ app/src/main/java/in/asclepius/app/activities/BookAppointment.kt
@@ -1,12 +1,140 @@
 package `in`.asclepius.app.activities
 
-import `in`.asclepius.app.R
-import androidx.appcompat.app.AppCompatActivity
+import `in`.asclepius.app.adapters.MemberAdapter
+import `in`.asclepius.app.databinding.ActivityBookAppointmentBinding
+import `in`.asclepius.app.models.AppUser
+import `in`.asclepius.app.others.Constants
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 
 class BookAppointment : AppCompatActivity() {
+
+    lateinit var binding: ActivityBookAppointmentBinding
+    lateinit var mContext: Context
+    lateinit var specializationSheet: BottomSheetBehavior<View>
+    lateinit var selectPatientSheet: BottomSheetBehavior<View>
+    lateinit var dateSelectionSheet: BottomSheetBehavior<View>
+    lateinit var timeSelectionSheet: BottomSheetBehavior<View>
+    val database = FirebaseDatabase.getInstance()
+    val userMembersReference = database.getReference(Constants.USER_MEMBERS_REFERENCE)
+    val firebaseUser = FirebaseAuth.getInstance().currentUser
+    val userMembers = mutableListOf<AppUser>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_book_appointment)
+
+        binding = ActivityBookAppointmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mContext = this
+
+        /* binding.selectPatient.firstPatientCard.setOnClickListener(View.OnClickListener {
+            setPatientSelected()
+        }) */
+
+        /*  binding.selectSpeciality.firstSpecialityCard.setOnClickListener(View.OnClickListener {
+              setSpecialitySelected()
+          })*/
+
+        binding.datePicker.dateNext.setOnClickListener(View.OnClickListener {
+            setDateSelected()
+        })
+
+        setPatientsRV()
+
+        specializationSheet = BottomSheetBehavior.from(binding.specialitySheet)
+        selectPatientSheet = BottomSheetBehavior.from(binding.patientSheet)
+        dateSelectionSheet = BottomSheetBehavior.from(binding.dateAndTimeSheet)
+        timeSelectionSheet = BottomSheetBehavior.from(binding.timeSheet)
+
+        timeSelectionSheet.state = BottomSheetBehavior.STATE_HIDDEN
+        specializationSheet.state = BottomSheetBehavior.STATE_HIDDEN
+        dateSelectionSheet.state = BottomSheetBehavior.STATE_HIDDEN
+        binding.dateAndTimeSheetLayout.visibility = View.VISIBLE
+        binding.specialitySheetLayout.visibility = View.VISIBLE
+        binding.timeSheetLayout.visibility = View.VISIBLE
+
+        Handler().postDelayed(Runnable {
+            selectPatientSheet.state = BottomSheetBehavior.STATE_EXPANDED
+        }, 500);
+
     }
+
+
+    private fun setPatientSelected() {
+        binding.selectPatient.selectedCard.root.visibility = View.VISIBLE
+        //binding.selectPatient.patientsLayout.visibility=View.GONE
+        specializationSheet.state = BottomSheetBehavior.STATE_EXPANDED
+        binding.specialitySheetLayout.visibility = View.VISIBLE
+        selectPatientSheet.isHideable = false
+    }
+
+    private fun setSpecialitySelected() {
+        binding.selectSpeciality.selectedCard.root.visibility = View.VISIBLE
+        binding.selectSpeciality.specialityLayout.visibility = View.GONE
+        dateSelectionSheet.state = BottomSheetBehavior.STATE_EXPANDED
+        selectPatientSheet.state = BottomSheetBehavior.STATE_EXPANDED
+        selectPatientSheet.isDraggable = false
+        specializationSheet.isDraggable = false
+        binding.dateAndTimeSheetLayout.visibility = View.VISIBLE
+        selectPatientSheet.isHideable = false
+    }
+
+    private fun setDateSelected() {
+        /* binding.dateAndTimeSheetLayout.selectedCard.visibility= View.VISIBLE
+         binding.dateAndTimeSheetLayout.datePickerLayout.visibility= View.GONE*/
+        timeSelectionSheet.state = BottomSheetBehavior.STATE_EXPANDED
+        selectPatientSheet.state = BottomSheetBehavior.STATE_EXPANDED
+        binding.dateAndTimeSheetLayout.visibility = View.VISIBLE
+        dateSelectionSheet.isDraggable = false
+        timeSelectionSheet.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    private fun setPatientsRV() {
+        firebaseUser?.uid?.let {
+            userMembersReference.child(it).addChildEventListener(object : ChildEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                    val user = snapshot.getValue(AppUser::class.java)
+                    user?.let { it1 -> userMembers.add(it1) }
+                    setPatientsAdapter()
+                }
+
+                override fun onChildRemoved(snapshot: DataSnapshot) {
+                    TODO("Not yet implemented")
+                }
+            })
+        }
+
+    }
+
+    private fun setPatientsAdapter() {
+        val memberAdapter = MemberAdapter(userMembers, this)
+        binding.selectPatient.patientsRV.layoutManager = LinearLayoutManager(this)
+        binding.selectPatient.patientsRV.adapter = memberAdapter
+    }
+
+
 }
---
+++ app/src/main/java/in/asclepius/app/adapters/MemberAdapter.kt
@@ -0,0 +1,45 @@
+package `in`.asclepius.app.adapters
+
+import `in`.asclepius.app.R
+import `in`.asclepius.app.databinding.MemberCardBinding
+import `in`.asclepius.app.models.AppUser
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MemberAdapter(
+    private val members: List<AppUser>,
+    private val context: Context
+) :
+    RecyclerView.Adapter<MemberAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val binding = MemberCardBinding.bind(itemView)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MemberAdapter.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.member_card, parent, false)
+        return MemberAdapter.ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return members.size
+    }
+
+    override fun getItemId(position: Int): Long {
+        return super.getItemId(position)
+    }
+
+    override fun onBindViewHolder(holder: MemberAdapter.ViewHolder, position: Int) {
+        val member = members.get(position)
+        holder.binding.memberName.text = member.fullName
+        holder.binding.realtionText.text =
+            context.getString(R.string.yours) + " " + member.relationType
+        holder.binding.memberAge.text = context.getString(R.string.age) + " " + member.relationType
+    }
+}
