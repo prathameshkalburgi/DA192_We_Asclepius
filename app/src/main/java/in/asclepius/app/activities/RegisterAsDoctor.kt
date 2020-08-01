8f121879401a7dd7817eedf33bb0996ea3f6a2e1
pspanishetti <praveenpanishetti@gmail.com>
2020-08-02 00:55:08 +0530

    [Feat] - File Uploading Feature is Added

--- app/src/main/java/in/asclepius/app/activities/RegisterAsDoctor.kt
+++ app/src/main/java/in/asclepius/app/activities/RegisterAsDoctor.kt
@@ -1,19 +1,22 @@
 package `in`.asclepius.app.activities
 
+import `in`.asclepius.app.R
+import `in`.asclepius.app.dailogs.LoadingDialog
 import `in`.asclepius.app.databinding.ActivityRegisterAsDoctorBinding
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
+import android.os.Handler
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import java.io.IOException
+import java.io.File
 
 
 class RegisterAsDoctor : AppCompatActivity() {
 
-    private lateinit var filePath: Uri
+    private var filePath: Uri? = null
     private val PICK_RESUME: Int = 1056
     lateinit var binding: ActivityRegisterAsDoctorBinding
 
@@ -24,20 +27,44 @@ class RegisterAsDoctor : AppCompatActivity() {
 
 
         binding.chooseResumeFile.setOnClickListener(View.OnClickListener {
+            selectResume()
+        })
 
+        binding.upload.setOnClickListener(View.OnClickListener {
+            if (binding.email.editText?.text.toString()
+                    .isBlank() || !binding.email.editText?.text.toString()
+                    .contains("@") || filePath == null
+            ) {
+                binding.email.error =
+                    getString(R.string.please_enter_correct_email_and_choose_resume)
+                binding.email.isErrorEnabled = true
+            } else {
+                val dailog = LoadingDialog(this@RegisterAsDoctor)
+                dailog.show()
+                dailog.setTitle(getString(R.string.uploading_details))
+
+                Handler().postDelayed(Runnable {
+                    dailog.dismiss()
+                    showSuccess()
+                }, 5000);
+            }
         })
 
     }
 
-    private fun SelectImage() {
+    private fun showSuccess() {
+        binding.successLayout.visibility = View.VISIBLE
+        binding.normalLayout.visibility = View.GONE
+    }
 
+    private fun selectResume() {
         val intent = Intent()
-        intent.type = "image/*"
+        intent.type = "file/*"
         intent.action = Intent.ACTION_GET_CONTENT
         startActivityForResult(
             Intent.createChooser(
                 intent,
-                "Select Image from here..."
+                "Select Resume Via..."
             ),
             PICK_RESUME
         )
@@ -45,18 +72,14 @@ class RegisterAsDoctor : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_RESUME && resultCode == Activity.RESULT_OK && data != null && data.data != null
-        ) {
-
-            try {
-                filePath = data.data!!
-                val bitmap = MediaStore.Images.Media
-                    .getBitmap(
-                        contentResolver,
-                        filePath
-                    )
-            } catch (e: IOException) {
-                e.printStackTrace()
+        if (requestCode == PICK_RESUME) {
+            if (resultCode == Activity.RESULT_OK) {
+                val selectedFileURI: Uri? = data?.data
+                val file = File(selectedFileURI?.path.toString())
+                Log.d("ResumeFile", "File : " + file.getName())
+                filePath = Uri.parse(file.getName().toString())
+                Log.d("ResumeFile", "FilePath : " + filePath)
+                binding.chooseText.text = file.name
             }
         }
     }
