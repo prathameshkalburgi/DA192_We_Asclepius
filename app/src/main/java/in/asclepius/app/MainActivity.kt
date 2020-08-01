b9b20cfc1e7f4c9082689917253da6345cc9579a
SHRIYX <shrinidhimp222@gmail.com>
2020-08-01 13:19:09 +0530

    [Design] - Main Activity Partial Design Completed

--- app/src/main/java/in/asclepius/app/MainActivity.kt
+++ app/src/main/java/in/asclepius/app/MainActivity.kt
@@ -1,11 +1,30 @@
 package `in`.asclepius.app
 
+import `in`.asclepius.app.activities.BookAppointment
+import `in`.asclepius.app.databinding.ActivityMainBinding
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var mContext: Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mContext=this
+
+        binding.bookAppointmentCard.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(mContext, BookAppointment::class.java))
+        })
+
+
     }
 }
